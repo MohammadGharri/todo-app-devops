673ac24d7d7bccb478b61d30d1de2102282aae5a
mohammad <einstein.gharri@gmail.com>
2025-06-07 07:08:42 +0330

    Edite app.py for pep8

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
+
 app = Flask(__name__)
-CORS(app)  # اجازه دسترسی از همه جا
+CORS(app)
+
 
 @app.route("/api/todos")
 def get_todos():
@@ -10,7 +12,6 @@ def get_todos():
         {"id": 1, "title": "🔥 Learn Docker (updated)", "done": False},
         {"id": 2, "title": "Build a CI/CD pipeline", "done": False}
     ])
-
 
 
 if __name__ == "__main__":
